780f828a0b09916dd243af058dbe94936f9d0f3a
Raunak Gurud <raunakgurud2121@gmail.com>
2025-06-18 12:23:46 +0530

    feat: added integration and basic ui (#4)

--- corenode/src/router.ts
+++ corenode/src/router.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { pingName } from "./controller";
 const router = Router();
 
-router.get("/", pingName);
+router.post("/", pingName);
 
 export default router;
