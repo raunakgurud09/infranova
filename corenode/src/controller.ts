c36f667aaf4954977fa07082d748b96051dd7295
Raunak Gurud <raunakgurud2121@gmail.com>
2025-06-18 11:55:26 +0530

    chore: added test and actions (#2)
    * chore: added test and workflow
    
    * chore: added github workflow
    
    * Add package.json for CI

--- corenode/src/controller.ts
+++ corenode/src/controller.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 export const pingName = (req: Request, res: Response) => {
   try {
-    const body = req.body?.name;
-    res.json({ name: body?.name ?? "" });
+    const name = req.body?.name;
+    res.json({ name: name ?? "" });
   } catch (error) {
     console.error(error);
   }
